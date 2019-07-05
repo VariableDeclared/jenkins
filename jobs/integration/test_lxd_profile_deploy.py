4af468165b778ebe6e4f7fa6db22ff19de8ce98d
Peter J De Sousa <peter.de.sousa@canonical.com>
2019-07-05 13:36:56 +0100

    Removed upgrade tests for now
    Need to investigate how charms upgrading will work.

--- jobs/integration/test_lxd_profile_deploy.py
+++ jobs/integration/test_lxd_profile_deploy.py
@@ -53,66 +53,53 @@ LXD_PROFILE = {
 async def test_lxd_profile_deploy_force(model, charm_name, charm_version):
     log('Deploying a the LXD charm')
 
-    # app = await model.deploy(
-    #     "cs:~containers/{}-{}".format(
-    #         charm_name,
-    #         charm_version
-    #     )
-    # )
-    # ##### CHANGE: Testing purposes.
-    app = await model.deploy(charm_name, force=False)
-    # #####
-    time.sleep(20)
-    log('waiting...')
-    asyncify(_juju_wait)
+    charm_names = ['kubernetes-worker', 'kubernetes-master']
+    for name in charm_names:
+        app = model.application[name]
 
-    machine = app.units[0]
-    log('app_info %s' % machine.safe_data)
+        machine = app.units[0]
+        log('app_info %s' % machine.safe_data)
 
-    model_name = 'juju-{}-{}-{}'.format(
-        os.environ['MODEL'],
-        machine.safe_data['application'],
-        machine.safe_data['name'].split('/')[1]
-    )
-    log('Checking model for name: %s' % model_name)
-    # In 3.7 stdout=subprocess.PIPE
-    # can be replaced with capture_output... :-]
-    result = subprocess.run(
-        ['lxc', 'profile', 'show', model_name],
-        stdout=subprocess.PIPE
-    )
+        model_name = 'juju-{}-{}-{}'.format(
+            os.environ['MODEL'],
+            machine.safe_data['application'],
+            machine.safe_data['name'].split('/')[1]
+        )
+        log('Checking model for name: %s' % model_name)
+        # In 3.7 stdout=subprocess.PIPE
+        # can be replaced with capture_output... :-]
+        result = subprocess.run(
+            ['lxc', 'profile', 'show', model_name],
+            stdout=subprocess.PIPE
+        )
 
-    config = result.stdout.decode('utf-8')
+        config = result.stdout.decode('utf-8')
 
-    loaded_yaml = yaml.load(config)
+        loaded_yaml = yaml.load(config)
 
-    # Remove these keys as they differ at run time and are
-    # not related to the configuration.
-    loaded_yaml.pop("name", None)
-    loaded_yaml.pop("used_by", None)
+        # Remove these keys as they differ at run time and are
+        # not related to the configuration.
+        loaded_yaml.pop("name", None)
+        loaded_yaml.pop("used_by", None)
 
-    log('Deployed Profile: %s' % loaded_yaml)
-    log('Expected Profile: %s' % LXD_PROFILE)
-    assert loaded_yaml == LXD_PROFILE
-    # .split(/) -- TESTING -REMOVE
-    await model.applications[charm_name.split('/')[-1]].destroy()
+        log('Deployed Profile: %s' % loaded_yaml)
+        log('Expected Profile: %s' % LXD_PROFILE)
+        assert loaded_yaml == LXD_PROFILE
 
-    log('Waiting...')
-    asyncify(_juju_wait)
 
-    async def test_deploy_fail(model):
-        # await model.deploy(
-        #     "cs:~containers/{}-{}".format(
-        #         charm_name,
-        #         charm_version
-        #     ),
-        #   force=False
-        # )
-        # ##### CHANGE: Testing purposes.
-        await model.deploy(charm_name, force=False)
-        # #####
-    with pytest.raises(JujuError, match=r"$[a-zA-Z]* invalid lxd-profile [a-zA-Z]*"):
-        await test_deploy_fail(model)
+    # async def test_deploy_fail(model):
+    #     # await model.deploy(
+    #     #     "cs:~containers/{}-{}".format(
+    #     #         charm_name,
+    #     #         charm_version
+    #     #     ),
+    #     #   force=False
+    #     # )
+    #     # ##### CHANGE: Testing purposes.
+    #     await model.deploy(charm_name, force=False)
+    #     # #####
+    # with pytest.raises(JujuError, match=r"$[a-zA-Z]* invalid lxd-profile [a-zA-Z]*"):
+    #     await test_deploy_fail(model)
 
 
 @pytest.mark.asyncio
@@ -132,6 +119,3 @@ async def test_lxd_profiles(log_dir):
             "/home/pjds/charms/builds/kubernetes-worker",
             os.environ['CHARM_VERSION']
         )
-
-
-def test_lxd_profile_upgrade
